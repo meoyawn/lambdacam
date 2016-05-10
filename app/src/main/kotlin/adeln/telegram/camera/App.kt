d807fb724da69904b5ee5e6e141091b4f46a771a
adelnizamutdinov <stiggpwnz@gmail.com>
2016-05-10 16:07:13 +0300

    rm bullshit

--- app/src/main/kotlin/adeln/telegram/camera/App.kt
+++ app/src/main/kotlin/adeln/telegram/camera/App.kt
@@ -2,11 +2,9 @@ package adeln.telegram.camera
 
 import android.app.Application
 import android.os.StrictMode
-import com.crashlytics.android.core.CrashlyticsCore
 import com.squareup.leakcanary.LeakCanary
 import common.android.threadPolicy
 import common.android.vmPolicy
-import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class App : Application() {
@@ -17,9 +15,6 @@ class App : Application() {
       StrictMode.setThreadPolicy(threadPolicy())
       Timber.plant(Timber.DebugTree())
       LeakCanary.install(this)
-    }
-    BACKGROUND_THREAD.execute {
-      Fabric.with(this, CrashlyticsCore())
     }
   }
 }
